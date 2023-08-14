9f61d54834b09ac27f774017199ae76cf3efd7f5
Nightsee <younes.naitsi.2002@gmail.com>
2023-08-14 08:31:43 -0700

    fix-10

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,23 +11,23 @@ from models.review import Review
 
 
 class FileStorage:
-    """ This is a storage engine for AirBnB clone project
-    Class Methods:
-        all: Returns the object
-        new: updates the dictionary id
-        save: Serializes, or converts Python objects into JSON strings
-        reload: Deserializes, or converts JSON strings into Python objects.
-    Class Attributes:
-        __file_path (str): The name of the file to save objects to.
-        __objects (dict): A dictionary of instantiated objects.
-        class_dict (dict): A dictionary of all the classes.
     """
-
+    This is a storage engine for AirBnB clone project
+    Class Methods:
+    all: Returns the object
+    new: updates the dictionary id
+    save: Serializes, or converts Python objects into JSON strings
+    reload: Deserializes, or converts JSON strings into Python objects.
+    Class Attributes:
+    __file_path (str): The name of the file to save objects to.
+    __objects (dict): A dictionary of instantiated objects.
+    class_dict (dict): A dictionary of all the classes.
+    """
     __file_path = 'file.json'
     __objects = {}
+
     class_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
-                  "Amenity": Amenity, "City": City, "Review": Review,
-                  "State": State}
+            "Amenity": Amenity, "City": City, "Review": Review, "State": State}
 
     def all(self):
         '''Return dictionary of <class>.<id> : object instance'''
@@ -42,11 +42,10 @@ class FileStorage:
     def save(self):
         """Save/serialize obj dictionaries to json file"""
         obj_dict = {}
-
         for key, obj in self.__objects.items():
             obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(obj_dict, f)
+            with open(self.__file_path, 'w', encoding="UTF-8") as f:
+                json.dump(obj_dict, f)
 
     def reload(self):
         """Deserialize/convert obj dicts back to instances, if it exists"""
